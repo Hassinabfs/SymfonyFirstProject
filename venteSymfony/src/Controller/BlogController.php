dd87add20ed4133c2c985e7f98b629be4543764f
BOUFATIS Hassina <boufatishassina@gmail.com>
2020-12-08 18:52:54 +0100

    creation de produit

---
+++ venteSymfony/src/Controller/BlogController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Produit::class);
+        /** je veux discuter a doctrine qui va me donner un repository */
+        $produits = $repo->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'produits' => $produits,
+        ]);
+    }
+
+
+    /**
+     * @Route("/",name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('blog/home.html.twig');
+    }
+    /**
+     * @Route("/blog/new",name="blog_ajout")
+     * @Route("/blog/{id}/edit",name="blog_edit")
+     */
+    public function AMProduit(Produit $produit = null, Request $request, EntityManagerInterface $manager)
+    {
+        if (!$produit) {
+            $produit = new Produit();
+        }
+
+        $form = $this->createFormBuilder($produit)
+            /**cette methode elle va creer un form qui va etre lier a mon article
+         * pour le configurer faut lui donner des champs qu on a besoin avec add
+         */
+            ->add('nom')
+            ->add('prix')
+            ->add('image')
+            ->add('categorie')
+            ->add('type')
+            ->add('description')
+            ->add('description_detaille')
+            ->getForm();
+
+        $form->handleRequest($request);
+        /**demander au formulaire d'analyser la requete http qu'on a envoyer en parametre  */
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $manager->persist($produit);
+            /**demander au manager de se preparer a faire persister l'article */
+            $manager->flush();
+            /**demander au manager de balancer la requete  */
+            return $this->redirectToRoute('blog_show', ['id' => $produit->getId()]);
+            /**demande de redirection a la page blog_show en precisant que l'id est celui de l' article ajouter */
+        }
+
+
+        return $this->render('blog/ajoutProduit.html.twig', [
+            'formProduit' => $form->createView(),
+            /**on va passer a twig un tableau qui contien les information qu on veut lui passer
+             * et egalement une variable formProduit  qui va avoir le resultat de createView de formulaire*/
+            'editMode' => $produit->getId() !== null
+        ]);
+    }
+    /**
+     * @Route("/blog/{id}",name="blog_show")
+     */
+    public function show($id): Response
+    {
+        $repo = $this->getDoctrine()->getRepository(Produit::class);
+        $produit = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'produit' => $produit
+        ]);
+    }
+}
---
+++ venteSymfony/src/Entity/Produit.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProduitRepository::class)
+ */
+class Produit
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $categorie;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descriptionDetaille;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrix(): ?string
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(string $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDescriptionDetaille(): ?string
+    {
+        return $this->descriptionDetaille;
+    }
+
+    public function setDescriptionDetaille(string $descriptionDetaille): self
+    {
+        $this->descriptionDetaille = $descriptionDetaille;
+
+        return $this;
+    }
+}
