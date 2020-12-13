65dcc9f932513686a24304f4461b37e879d52323
BOUFATIS Hassina <boufatishassina@gmail.com>
2020-12-13 03:51:26 +0100

    modifier les utilisateurs

--- venteSymfony/src/Controller/AdminController.php
+++ venteSymfony/src/Controller/AdminController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\EditUseType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
+
+
+
 
 
 
@@ -13,7 +21,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin/", name="admin_index")
+     * @Route("/admin", name="admin_index")
      */
     public function index(): Response
     {
@@ -29,6 +37,32 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'users' => $users->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin/users/edit/{id}", name="admin_userEdit")
+     */
+    public function usersEdit(User $user, Request $request): Response
+    {
+        $form = $this->createForm(EditUseType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+
+            $this->addFlash('message', 'utilisateur modifier avec succes');
+
+
+            return $this->redirectToRoute('admin_user');
+        }
+        return $this->render('admin/userEdit.html.twig', [
+            'userForm' => $form->createView()
         ]);
     }
 }
